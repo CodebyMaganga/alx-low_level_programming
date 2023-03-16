ac99623da74561a921dcb51814366bf34a8a5208
CodebyMaganga <codebymaganga@gmail.com>
2023-03-16 04:59:51 -0700

    Fix4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+ * main-prints Programming is like building a multilingual puzzle
+ * return-functions returns 0 if susuccesful
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }		
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * This is a comment
+ */
+int main(void)
+{
+printf("Size of a char: %d", sizeof(char));
+printf("Size of an integer: %d", sizeof(int));
+printf("Size of a long int: %d", sizeof(long int));
+printf("Size of a long long int: %d", sizeof(long long int));
+printf("Size of a float: %d", sizeof(float));
+return (0);
+}
