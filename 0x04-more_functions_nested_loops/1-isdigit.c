b9fa83d7c870d62576ec49b174d3d59aef5084e3
CodebyMaganga <codebymaganga@gmail.com>
2023-03-24 14:48:47 -0700

    Add 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * print_numbers- checks for digit 0-9
+ * _isdigit- checks for digit 0-9
+ * @c: char to be checked
  * Return: 0 if succesful
  */
-void print_numbers(void)
+int _isdigit(int c)
 {
-	char c;
-
-	for (c == '0' ; c <= '9'; c++)
-{
-		_putchar(c);
-}
-		_putchar('\n');
+	if (c == '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
