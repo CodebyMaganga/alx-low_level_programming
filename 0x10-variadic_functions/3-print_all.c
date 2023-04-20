ec400b25e2c917512011f433915f64bf02739646
codebymaganga <codebymaganga@gmail.com>
2023-04-20 14:55:23 -0700

    fix task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,52 +7,39 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0;
-	char c;
+	char *str, *sep = "";
+
 	va_list print;
 
 	va_start(print, format);
 
-	while ((c = format[i]) != '\0')
+	if (format)
 	{
-		if (c == 'c')
+		while (format[i])
 		{
-			char ch = (char) va_arg(print, int);
-
-			printf("%c", ch);
-		}
-		else if (c == 'i')
-		{
-			int num = va_arg(print, int);
-
-			printf("%d", num);
-		}
-		else if (c == 'f')
-		{
-			double f = va_arg(print, double);
-
-			printf("%f", f);
-		}
-		else if (c == 's')
-		{
-			char *s = va_arg(print, char *);
-
-			if (s != NULL)
+			switch (format[i])
 			{
-				printf("%s", s);
+				case 'c':
+					printf("%s%c", sep, va_arg(print, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(print, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(print, double));
+					break;
+				case 's':
+					str = va_arg(print, char *);
+					if (str == NULL)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+						i++;
+						continue;
 			}
-			else
-			{
-				printf("(nil)");
-			}
+			sep = ", ";
+			i++;
 		}
-		if (format[i + 1] != '\0' && (c == 'c' || c == 'i' || c == 'f' || c == 's'))
-		{
-			printf(", ");
-		}
-		i++;
 	}
-	va_end(print);
-	printf("\n");
 }
-
-
